79ddde7cea7f3d79fed78b5c828fa619beb122ce
Rajesh Dewle <rajesh_dewle@hotmail.com>
2020-03-20 15:21:34 +0530

    ⚡ Added users data in users variable

--- app/Http/Livewire/Search.php
+++ app/Http/Livewire/Search.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Livewire;
 
+use App\User;
 use Livewire\Component;
 
 class Search extends Component
 {
     public $query;
+    public $users;
 
+    public function mount()
+    {
+        $this->reset();
+    }
     public function render()
     {
+        $this->users = User::where('name', 'like', '%' . $this->query . '%')->get();
+
         return view('livewire.search');
     }
 }
