56d9018663cc4efbab3c6bac796fcf207574c38d
Rajesh Dewle <rajesh_dewle@hotmail.com>
2020-03-20 15:22:00 +0530

    ⚡ Added list of users name

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,3 +1,13 @@
 <div>
     <input type="text" placeholder="Search users" class="border w-full mt-12 p-6 mw-xl mx-auto" wire:model="query">
+
+    <ul>
+        @if (!empty($query))
+            @if (count($users) >= 1)
+                @foreach ($users as $user)
+                    <li class="p-2">{{ $user->name }}</li>
+                @endforeach
+            @endif
+        @endif
+    </ul>
 </div>
