a6adf19d5995eaeea85d141b4ebcdc76cda833f9
Rajesh Dewle <rajesh_dewle@hotmail.com>
2020-03-26 18:01:33 +0530

    ✨ Added function for arrow key up/down and select user profile

--- app/Http/Livewire/Search.php
+++ app/Http/Livewire/Search.php
@@ -9,11 +9,37 @@ class Search extends Component
 {
     public $query;
     public $users;
+    public $current;
 
     public function mount()
     {
         $this->reset();
+        $this->current = -1;
     }
+
+    public function down()
+    {
+        $this->current++;
+
+        if ($this->users->count() === $this->current) {
+            $this->current = -1;
+        }
+    }
+
+    public function up()
+    {
+        $this->current--;
+
+        if ( -1 > $this->current) {
+            $this->current = $this->users->count() - 1;
+        }
+    }
+
+    public function selectUser()
+    {
+        return redirect()->route('profile', $this->users[$this->current]->id);
+    }
+
     public function render()
     {
         $this->users = User::where('name', 'like', '%' . $this->query . '%')->get();
