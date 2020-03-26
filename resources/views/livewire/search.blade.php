e585758af1343e9c48bbf9e188983df2564d9418
Rajesh Dewle <rajesh_dewle@hotmail.com>
2020-03-26 18:00:25 +0530

    ⚡ Added link for user list

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,13 +1,16 @@
 <div>
-    <input type="text" placeholder="Search users" class="border w-full mt-12 p-6 mw-xl mx-auto" wire:model="query">
+    <input type="text" placeholder="Search users" class="border w-full mt-12 p-6 mw-xl mx-auto" wire:model="query"
+        wire:keydown.arrow-down="down" wire:keydown.arrow-up="up" wire:keydown.enter="selectUser">
 
     <ul>
         @if (!empty($query))
-            @if (count($users) >= 1)
-                @foreach ($users as $user)
-                    <li class="p-2">{{ $user->name }}</li>
-                @endforeach
-            @endif
+        @if (count($users) >= 1)
+        @foreach ($users as $user)
+        <li class="p-2 hover:bg-blue-100 {{ $current === $loop->index ? 'bg-blue-100' : ''}}">
+            <a href="/profile/{{ $user->id }}">{{ $user->name }}</a>
+        </li>
+        @endforeach
+        @endif
         @endif
     </ul>
 </div>
